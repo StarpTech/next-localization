12668f48ff62d17033561d4a273fb1024e172cce
starptech <deusdustin@gmail.com>
2020-08-30 16:16:24 +0200

    fix hook dep

--- src/hooks/use-plural.js
+++ src/hooks/use-plural.js
@@ -1,9 +1,13 @@
 import { useMemo } from 'react';
+
 import useI18n from './use-i18n';
 
 export default function usePlural(bcp47Tag, pluralRulesOptions = { type: 'ordinal' }) {
     const i18n = useI18n();
-    const PR = useMemo(() => new Intl.PluralRules(bcp47Tag, pluralRulesOptions), [bcp47Tag]);
+    const PR = useMemo(() => new Intl.PluralRules(bcp47Tag, pluralRulesOptions), [
+        bcp47Tag,
+        pluralRulesOptions
+    ]);
 
     return (key, params) => {
         Object.keys(params).map((k) => {
