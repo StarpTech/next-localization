2f571a7da1b6778d521f474a1979c8df068642af
Frederick Morin <frederickmorin18@gmail.com>
2020-09-07 17:10:47 -0400

    Fix I18nProvider type test
    by providing a component as children

--- types/index.test-d.tsx
+++ types/index.test-d.tsx
@@ -1,6 +1,14 @@
 import * as React from 'react';
 import { I18nProvider } from '.';
 
+function Component() {
+    return null;
+}
+
 export default function App() {
-    return <I18nProvider locale="de" lngDict={{}} />;
+    return (
+        <I18nProvider locale="de" lngDict={{}}>
+            <Component />
+        </I18nProvider>
+    );
 }
