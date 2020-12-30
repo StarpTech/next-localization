adcdcdbeafee30a4ac051254ce143fc100b74930
Dustin Deus <deusdustin@gmail.com>
2020-12-30 12:38:44 +0100

    update dev dependencies, remove eslint from dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,7 +31,8 @@ module.exports = {
         'react/react-in-jsx-scope': 'off',
         'react/prop-types': 'off',
         '@typescript-eslint/explicit-function-return-type': 'off',
-        'simple-import-sort/sort': 'error',
+        'simple-import-sort/imports': 'error',
+        'simple-import-sort/exports': 'error',
         'jsx-a11y/anchor-is-valid': [
             'error',
             {
